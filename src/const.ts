81809c1468e10b792fb4fafd65f8fe09c822b676
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2023-12-31 11:37:58 -0500

    feat: add media

--- src/const.ts
+++ src/const.ts
@@ -1,56 +1,50 @@
-// export const PRODUCTS = [
-//     {
-//         name: "watched v2",
-//         image:
-//           "https://framerusercontent.com/images/ML9zOhQEKk8lRvGwMTwOadrXqmE.jpg",
-//         links: {
-//           github: "https://github.com/joshuaakanetuk/watchd",
-//           demo: "https://trywatchd.com/",
-//         },
-//         description:
-//           "Conquer your watchlist one movie at a time! Movie watching app (clone of Letterboxd).",
-//         tags: [
-//           "Typescript",
-//           "React",
-//           "Postgres",
-//           "Quirrel (Serverless Queuing)",
-//           "Javascript",
-//           "TMDB API (API Integration)",
-//         ],
-//       },
-//   {
-//     name: "watched v1",
-//     image:
-//       "https://framerusercontent.com/images/ML9zOhQEKk8lRvGwMTwOadrXqmE.jpg",
-//     links: {
-//       github: "",
-//       demo: "https://watched.joshuaakanetuk.com/",
-//     },
-//     description:
-//       "A Chrome extension boilerplated with Jonghakseo/chrome-extension-boilerplate-react-vite",
-//     tags: [
-//       "Javascript",
-//       "TMDB API (API Integration)",
-//     ],
-//   },
-//   {
-//     name: "Clipper",
-//     image:
-//       "https://framerusercontent.com/images/ML9zOhQEKk8lRvGwMTwOadrXqmE.jpg",
-//     links: {
-//       github: "",
-//       demo: "",
-//     },
-//     description:
-//       "A Chrome extension boilerplated with Jonghakseo/chrome-extension-boilerplate-react-vite",
-//     tags: [
-//       "Typescript",
-//       "React",
-//       "Browser Extension",
-//       "Javascript",
-//       "Notion API (API Integration)",
-//     ],
-//   },
-// ]
-
-export const PRODUCTS = [];
+export const PRODUCTS = [
+  {
+    name: "watched v2",
+    image:
+      "https://iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.work/watched2.mp4",
+    links: {
+      github: "https://github.com/joshuaakanetuk/watchd",
+      demo: "https://trywatchd.com/",
+    },
+    description:
+      "Conquer your watchlist one movie at a time! Movie watching app (clone of Letterboxd).",
+    tags: [
+      "Typescript",
+      "React",
+      "Postgres",
+      "Quirrel (Serverless Queuing)",
+      "Javascript",
+      "TMDB API (API Integration)",
+    ],
+  },
+  {
+    name: "watched v1",
+    image:
+      "https://iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.work/watched.mp4",
+    links: {
+      github: "https://github.com/joshuaakanetuk/watched",
+      demo: "https://watched.joshuaakanetuk.com/",
+    },
+    description:
+      "Log and track all of the movies you watch. For movie fanatics. Vanilla Javascript clone of popular movie logging app, Letterboxd.",
+    tags: ["Javascript", "TMDB API (API Integration)"],
+  },
+  {
+    name: "Clipper",
+    image:
+      "https://framerusercontent.com/images/ML9zOhQEKk8lRvGwMTwOadrXqmE.jpg",
+    links: {
+      github: "https://github.com/joshuaakanetuk/clipper",
+    },
+    description:
+      "A Chrome extension boilerplated with Jonghakseo/chrome-extension-boilerplate-react-vite",
+    tags: [
+      "Typescript",
+      "React",
+      "Browser Extension",
+      "Javascript",
+      "Notion API (API Integration)",
+    ],
+  },
+];
